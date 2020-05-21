2d651483621e34cbc07d0c6da17d50c898fc39fb
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-21 20:33:00 +1000

    add Chart to Weather

--- src/Containers/Weather/Weather.js
+++ src/Containers/Weather/Weather.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Chart from '../../Components/Chart/Chart';
+import './style.css';
 
 const Weather = (props) => {
+	const getWeatherData = (weatherData, weatherProperty) => {
+		return weatherData.map((item) => item.main[weatherProperty]);
+	};
+
 	const renderWeather = () => {
-		return props.weather.map((weatherItem) => {
-			const { id, name } = weatherItem.city;
+		return props.weather.map(({ city, list }) => {
+			const { id, name } = city;
+			const temps = getWeatherData(list, 'temp');
+			const humidity = getWeatherData(list, 'humidity');
+			const pressure = getWeatherData(list, 'pressure');
 			return (
 				<tr key={id}>
 					<td>{name}</td>
+					<td>
+						<Chart color="red" chartData={temps} unit="K" />
+					</td>
+					<td>
+						<Chart color="blue" chartData={humidity} unit="%" />
+					</td>
+					<td>
+						<Chart color="green" chartData={pressure} unit="hPa" />
+					</td>
 				</tr>
 			);
 		});
@@ -18,9 +36,9 @@ const Weather = (props) => {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temp.</th>
-						<th>Press.</th>
-						<th>Hum.</th>
+						<th>Temp. (K)</th>
+						<th>Press. (hPa)</th>
+						<th>Hum. (%)</th>
 					</tr>
 				</thead>
 				<tbody>{renderWeather()}</tbody>
